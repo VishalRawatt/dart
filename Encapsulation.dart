dea703e1919bcdc4829940d7d02c8df6471b03eb
Vishal Rawat <94183896+VishalRawatt@users.noreply.github.com>
2024-03-17 13:17:44 +0530

    Inheritance

--- Encapsulation.dart
+++ Encapsulation.dart
@@ -3,29 +3,24 @@ class Employee {
   int? _id;
   String? _name;
 
-// Getter method to access private property _id
   int getId() {
-    return _id!; //! means we are forcing that the value should not be there
+    return _id!;
   }
 
-// Getter method to access private property _name
   String getName() {
     return _name!;
   }
 
-// Setter method to update private property _id
   void setId(int id) {
     this._id = id;
   }
 
-// Setter method to update private property _name
   void setName(String name) {
     this._name = name;
   }
 }
 
 void main() {
-  // Create an object of Employee class
   Employee emp = new Employee();
   emp.setId(1);
   emp.setName("John");
